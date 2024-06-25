636ec088477a00fac79d4d59ccf219fa432bdd3d
lindakwoo <lindakwoo@gmail.com>
2024-06-25 13:26:00 -0700

    add functionality to do one round at a time

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from schedule.views import list_games, regenerate_schedule
+from schedule.views import list_games
 
 urlpatterns = [
     path("<int:id>/", list_games, name="list_games"),
-    path("<int:id>/regenerate/<int:num_courts>/",
-         regenerate_schedule, name="regenerate_schedule"),
+
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,18 +1,16 @@
-from django.shortcuts import render, get_object_or_404
-from .schedule_generator import generate_schedule
+from django.shortcuts import render, get_object_or_404, redirect
+from .schedule_generator import generate_round
 from players_lists.models import PlayersList
 from django.contrib.auth.decorators import login_required
 
 
-def get_schedule(courts, players):
-    schedule = generate_schedule(courts, players)
+def get_next_round(courts, players, games_counter, team_history):
+    result = generate_round(courts, players, games_counter, team_history)
 
   # Process the schedule to match template expectations
-    processed_schedule = []
-    for round_games in schedule:
+    if result:
+        round_games, sitting_out, games_counter, team_history = result
         processed_round = []
-
-        sitting_out = round_games.pop()  # The last item is the sitting out players
         for game in round_games:
             processed_game = {
                 "team1_player1": game.team1[0],
@@ -22,42 +20,57 @@ def get_schedule(courts, players):
             }
 
             processed_round.append(processed_game)
-        processed_schedule.append({
-            "games": processed_round,
-            "sitting_out": sitting_out
-        })
-    return processed_schedule
+        return {"games": processed_round, "sitting_out": sitting_out}, games_counter, team_history
+
+    return None
 
 
 @login_required
 def list_games(request, id):
     players_list = get_object_or_404(PlayersList, id=id)
-    num_courts = request.POST.get('num_courts')
-    title = players_list.title
-
-    players = [player.name for player in players_list.players.all()]
-    courts = int(num_courts)
-
-    schedule = get_schedule(courts, players)
-
-    context = {
-        "schedule": schedule,
-        "list_title": title,
-        "num_courts": courts,
-        "list_id": id
-    }
-    return render(request, 'schedule/list_games.html', context)
-
-
-@login_required
-def regenerate_schedule(request, id, num_courts):
-    players_list = get_object_or_404(PlayersList, id=id)
     title = players_list.title
     players = [player.name for player in players_list.players.all()]
-    schedule = get_schedule(num_courts, players)
+    max_attempts = 100
+
+    if request.method == "POST":
+        if "create_schedule" in request.POST:
+            num_courts = request.POST.get('num_courts')
+            courts = int(num_courts)
+            # Clear session data for a new schedule
+            request.session["games_counter"] = [0] * len(players)
+            request.session["team_history"] = []
+            request.session["rounds"] = []
+            request.session["num_courts"] = courts
+            return redirect('list_games', id=id)
+
+    games_counter = request.session["games_counter"]
+    team_history = request.session["team_history"]
+    num_courts = request.session["num_courts"]
+
+    for counter in range(max_attempts):
+        print(counter)
+
+        result = get_next_round(
+            num_courts, players, games_counter, team_history)
+
+        if result:
+            next_round, games_counter, team_history = result
+            break
+    if not result:
+        request.session["team_history"] = []
+        team_history = request.session["team_history"]
+        result = get_next_round(
+            num_courts, players, games_counter, team_history)
+        if result:
+            next_round, games_counter, team_history = result
+
+    if result:
+        request.session["games_counter"] = games_counter
+        request.session["team_history"] = team_history
+        request.session["rounds"].append(next_round)
 
     context = {
-        "schedule": schedule,
+        "schedule": request.session["rounds"],
         "list_title": title,
         "num_courts": num_courts,
         "list_id": id
